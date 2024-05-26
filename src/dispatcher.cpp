337f0a916ed11265e4d607fe60d04ea2ef496d23
Aleksa Mrda <aleksamrda@gmail.com>
2024-05-26 23:48:19 +0200

    fix bug(close socket connection)

--- src/dispatcher.cpp
+++ src/dispatcher.cpp
@@ -1,16 +1,28 @@
 #include "dispatcher.hpp"
 #include <sys/socket.h>
+#include <unistd.h>
 #include <cstdio>
+#include <cstring>
 #include <iostream>
 #include <string>
 
+// TODO:
 void take_over(SOCKET_FD socket) {
     std::cout << "Hello, world from dispatcher\n";
-    char        buffer[4096] = {0};
+
+    char        buffer[4096];
+    ssize_t     bytes_received;
     std::string request;
-    while (recv(socket, buffer, 4096, 0) > 0) {
-        request.append(buffer);
+
+    while ((bytes_received = recv(socket, buffer, sizeof(buffer), 0)) > 0) {
+        request.append(buffer, bytes_received);
+
+        memset(buffer, 0, sizeof(buffer));
     }
 
-    perror("hello");
+    // Check for errors or end-of-file
+    if (bytes_received < 0)
+        perror("recv failed");
+
+    close(socket);
 }
