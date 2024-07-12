e6fe103822c3409949668ce0ebfcfe3d3c7dd6a9
Stefan Crepulja <stefan.crepulja@gmail.com>
2024-07-12 23:21:19 +0200

    Testing and logging results of request parser

---
+++ src/dispatcher.cpp
@@ -0,0 +1,46 @@
+/**
+ * @file dispatcher.cpp
+ * @brief This file implements the dispatcher thread
+ *
+ * This function runs in a separate thread for each connection and is
+ * responsible for reading data from the socket, processing the request, and
+ * sending a response.
+ */
+
+#include "dispatcher.hpp"
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+#include <iostream>
+#include "req-parser.hpp"
+
+// TODO:
+void take_over(SOCKET_FD socket) {
+    std::cout << "Hello, world from dispatcher\n";
+
+    char        buffer[REQ_BUF_SIZE];
+    ssize_t     bytes_received;
+    std::string request;
+
+    // FIX: 1. optimizovati
+    //      2. ne prekida konekciju
+    while ((bytes_received = recv(socket, buffer, sizeof(buffer), 0)) > 0) {
+        request.append(buffer, bytes_received);
+
+        memset(buffer, 0, sizeof(buffer));
+    }
+
+    // Check for errors or end-of-file
+    if (bytes_received < 0) {
+        perror("recv failed");
+    }
+
+    close(socket);
+
+    std::cout << request;
+
+    RequestParser requestParser(request);
+    Request       newRequest = requestParser.parseRequest(request);
+    std::cout << "Method: " << newRequest.getMethodType() << std::endl;
+    std::cout << "Path : " << newRequest.getPath() << std::endl;
+}
---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include "constants.hpp"
+#include "path-and-type.hpp"
+#include "request.hpp"
+#include "router.hpp"
+#include "server.hpp"
+
+void mojafunc1() {
+    std::cout << "Hello pat 1\n";
+}
+
+void mojafunc2() {
+    std::cout << "Hello pat 2\n";
+}
+
+void mojafunc3(const Request&, const Response&) {
+    std::cout << "Hello pat 3\n";
+}
+
+void controller(const Request&, Response& res) {
+    res.status(OK);
+    std::cout << "Hello from controller\n";
+}
+
+int main() {
+    PathAndType pat;
+    PathAndType pat2("/home", HTTP_POST);
+    PathAndType pat3("/home", HTTP_PUT);
+    PathAndType pat4("/home", HTTP_POST);
+
+    /*std::unordered_map<PathAndType, std::function<void()>> mapa = {*/
+    /*    {pat, mojafunc1},*/
+    /*};*/
+
+    /*mapa[pat2]();*/
+
+    Router  router;
+    Request req;
+    req.setPathAndType(PathAndType());
+    Response res;
+
+    router.route(pat4, controller);
+    std::cout << req.getPathAndType().path;
+    std::cout << req.getPathAndType().method_type;
+
+    Server server;
+    server.start(5000);
+
+    return 0;
+}
