3b3b12ade14b454542d2d440ec56bfc94095561c
Aleksa Mrda <aleksamrda@gmail.com>
2024-12-20 16:03:04 +0100

    Refactor: use word 'handler' instead of 'controller' for clarity and consistency

--- include/router.hpp
+++ include/router.hpp
@@ -13,7 +13,7 @@
  * @brief Class used for routing
  *
  * Router uses std::unordered_map internally to map HTTP request method and path
- * to a developer defined controller function.
+ * to a developer defined handler function.
  */
 class Router {
 private:
@@ -21,7 +21,7 @@ private:
         PathAndType,
         std::function<void(const Request& req, Response& res)>>>
         routing_table; /**< Internal hashmap that associates HTTP methods and
-                          paths with controller functions. */
+                          paths with handler functions. */
 
     /**
      * Hashmap that maps file extensions to their respective MIME types. Only
@@ -45,62 +45,62 @@ public:
 
     /**
      * @brief Used internally to allow dispatcher to call appropriate
-     * controller.
+     * handler.
      *
-     * A method that will call the appropriate controller function based on the
+     * A method that will call the appropriate handler function based on the
      * provided PathAndType object. Used internally to allow dispatcher to call
-     * appropriate controller.
+     * appropriate handler.
      *
-     * @param pat PathAndType object used for finding appropriate controller
+     * @param pat PathAndType object used for finding appropriate handler
      * @param req Reference to Request object representing HTTP request
      * @param res Reference to Response object that is manipulated by developer
      */
     void call(const Request& req, Response& res) noexcept;
 
     /**
-     * @brief Maps an HTTP method and path to a controller function.
+     * @brief Maps an HTTP method and path to a handler function.
      *
-     * Associates a specified HTTP method and URL path with a controller
+     * Associates a specified HTTP method and URL path with a handler
      * function that will handle incoming requests matching the given method and
      * path.
      *
      * @param method_type The HTTP method (e.g., GET, POST).
      * @param path The URL path.
-     * @param controller The controller function to handle the requests.
+     * @param handler The handler function to handle the requests.
      */
-    void route(const MethodType method_type,
-               const char*      path,
+    void route(MethodType  method_type,
+               const char* path,
                const std::function<void(const Request& req, Response& res)>&
-                   controller) noexcept;
+                   handler) noexcept;
 
     /**
-     * @brief Same as Router::route(HTTP_GET, path, controller)
+     * @brief Same as Router::route(HTTP_GET, path, handler)
      *
      * @param path The URL path.
-     * @param controller The controller function to handle the requests.
+     * @param handler The handler function to handle the requests.
      */
     void get(const char* path,
              const std::function<void(const Request& req, Response& res)>&
-                 controller) noexcept;
+                 handler) noexcept;
 
     /**
-     * @brief Same as Router::route(HTTP_PUT, path, controller)
+     * @brief Same as Router::route(HTTP_PUT, path, handler)
      *
      * @param path The URL path.
-     * @param controller The controller function to handle the requests.
+     * @param handler The handler function to handle the requests.
      */
     void put(const char* path,
              const std::function<void(const Request& req, Response& res)>&
-                 controller) noexcept;
+                 handler) noexcept;
     /**
-     * @brief Same as Router::route(HTTP_POST, path, controller)
+     * @brief Same as Router::route(HTTP_POST, path, handler)
      *
      * @param path The URL path.
-     * @param controller The controller function to handle the requests.
+     * @param handler The handler function to handle the requests.
      */
     void post(const char* path,
               const std::function<void(const Request& req, Response& res)>&
-                  controller) noexcept;
+                  handler) noexcept;
 
     /**
      * @brief Configures the router to serve static files from a specified
@@ -134,7 +134,7 @@ private:
      * requested, additionally returns page not found document
      *
      */
-    void res_not_found(const PathAndType& pat, Response& res) noexcept;
+    void res_not_found(Response& res) const noexcept;
 
     /**
      * @brief Attempts to locate the requested file if no handler is available.
@@ -146,7 +146,7 @@ private:
      * @brief Verifies that the provided file path is both valid and points to
      * an existing file.
      */
-    bool is_req_file_legit(const std::filesystem::path& p) noexcept;
+    bool is_req_file_legit(const std::filesystem::path& p) const noexcept;
 
     /**
      * @brief if path/index.html exists map GET / to it
@@ -157,7 +157,7 @@ private:
      * @brief set date header
      *
      */
-    void set_date_header(Response& res) noexcept;
+    void set_date_header(Response& res) const noexcept;
 
     /**
      * @brief compress response if clients supports it.
--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,16 @@
 #define port 5000
 
 void hello_world(const Request& req, Response& res) {
-    res.send("Hello, world from controller function!!").status(OK);
+    res.send("Hello, world from handler function!!").status(OK);
+}
+
+void serve_file(const Request& req, Response& res) {
+    res.attachment("./public/index.html").status(OK);
 }
 
 void init_router(Router& router) {
-    router.route(HTTP_GET, "/controller", hello_world);
+    router.route(HTTP_GET, "/handler", hello_world);
+    router.get("/main", serve_file);
     router.serve_static("./public/");
     router.not_found("./public/notfound.html");
 }
