c30ac9e4bf6972657cb530fd1287a29899a94638
Aleksa Mrda <aleksamrda@gmail.com>
2025-01-29 01:48:03 +0100

    Add: Error class that will be used for error handling

--- include/http-error.hpp
+++ include/http-error.hpp
@@ -1,17 +1,89 @@
 #ifndef HTTP_ERROR
 #define HTTP_ERROR
+
 #include <string>
-#include <utility>
+#include "constants.hpp"
 #include "http-status-codes.hpp"
-/**
- * @class HttpError
- * @brief Serves purpose of standardizing and abstracting error handling
- *
- */
-struct HttpError {
+
+namespace super {
+
+class HttpError {
+private:
     HttpStatus  status;
-    std::string message;
-    std::string function_name;
-    HttpError(HttpStatus status_code, std::string func_name);
+    std::string message;  // Log message (not sent to client)
+    Headers     headers;
+
+public:
+    HttpError(HttpStatus status_code) : status(status_code) {}
+
+    HttpError(HttpStatus status_code, std::string new_message)
+        : status(status_code),
+          message(std::move(new_message)) {}
+
+    [[nodiscard]] constexpr HttpStatus get_status() const noexcept {
+        return status;
+    }
+
+    [[nodiscard]] const std::string& get_message() const noexcept {
+        return message;
+    }
+
+    [[nodiscard]] const Headers& get_headers() const noexcept {
+        return headers;
+    }
+
+    HttpError& add_header(const std::string& key,
+                          const std::string& value) noexcept {
+        if (key.empty() || value.empty()) {
+            return *this;
+        }
+        headers.emplace(key, value);
+        return *this;
+    }
+
+    HttpError& add_header(std::string&& key, std::string&& value) noexcept {
+        if (key.empty() || value.empty()) {
+            return *this;
+        }
+        headers.emplace(std::move(key), std::move(value));
+        return *this;
+    }
+
+    HttpError& add_headers(const Headers& new_headers) noexcept {
+        for (const auto& [key, value] : new_headers) {
+            if (key.empty() || value.empty()) {
+                continue;
+            }
+            headers.emplace(key, value);
+        }
+
+        return *this;
+    }
+
+    HttpError& add_headers(Headers&& new_headers) noexcept {
+        // Check for self-assignment
+        if (&headers == &new_headers) {
+            return *this;
+        }
+
+        // Reserve space in headers to avoid reallocations
+        headers.reserve(headers.size() + new_headers.size());
+
+        // Move elements from new_headers to headers, skipping empty keys or
+        // values
+        for (auto& pair : new_headers) {
+            if (pair.first.empty() || pair.second.empty()) {
+                headers.insert(std::move(pair));
+            }
+        }
+
+        // Optionally clear new_headers after moving its contents
+        new_headers.clear();
+
+        return *this;
+    }
 };
+
+}  // namespace super
+
 #endif
